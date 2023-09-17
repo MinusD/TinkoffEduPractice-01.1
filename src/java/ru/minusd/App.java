da3a1c7228971b527ee2bb861ecf7942eb3bce5b
MinusD <lukovnikov50@mail.ru>
2023-09-18 00:50:48 +0300

    init

---
+++ src/java/ru/minusd/App.java
@@ -0,0 +1,45 @@
+package src.java.ru.minusd;
+
+/**
+ * Используем volatile и synchronized, что бы потоки не конфликтовали
+ */
+public class App {
+    private static volatile int counter = 1;
+
+    private static synchronized void incrementCounter() {
+        counter++;
+    }
+
+    public static void main(String[] args) {
+        var th1 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                incrementCounter();
+            }
+        });
+        var th2 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                incrementCounter();
+            }
+        });
+        var th3 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                incrementCounter();
+            }
+        });
+        th1.start();
+        th2.start();
+        th3.start();
+
+        // Ждём пока потоки закончатся
+        try {
+            th1.join();
+            th2.join();
+            th3.join();
+        } catch (Exception e) {
+            System.out.println("Threads Error :(");
+        }
+
+
+        System.out.println("Counter value: " + counter);
+    }
+}
